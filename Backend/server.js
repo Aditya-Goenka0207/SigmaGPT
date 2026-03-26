b6beb5310c1cc3137064bde0ba4566a59285c2e6
Aditya-Goenka0207 <aditya.goenka0207@gmail.com>
2026-03-26 21:39:17 +0530

    Frontend url added

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: "*", //restrict to frontend URL later
+    origin: "http://localhost:5173", //restrict to frontend URL later
   }),
 );
 
